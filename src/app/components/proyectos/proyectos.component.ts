c7e5a7f6cd2237cbb04306e3d546c43a4633a529
LuisCarrero <luis.carreroy21@gmail.com>
2026-05-13 23:44:06 +0200

    feat: agregando componente de proyectos al portafolio

---
+++ src/app/components/proyectos/proyectos.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proyectos',
+  template: ` <h2>Mis Proyectos</h2> `,
+  standalone: true,
+})
+export class ProyectosComponent {}
